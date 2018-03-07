24788ddc7a5e5d909c2137a876ca567ad771af71
Andriy Golovko <andriy.golovko@jambit.com>
2018-03-07 09:40:01 +0100

    Remove unnecessary bash files

--- src/simple.py
+++ src/simple.py
@@ -51,12 +51,27 @@ def help():
 
 
 commands = {
-    "scrape": ["s", "Scrape new items, apply regression to it, dump them into CSV"],
+    "scrape": ["s", "Find new vehicles from the search URLs, defined in the '/dir-configs/searchpages.txt' file. " +
+               "\n" +
+               "After scraping of the web, the regression analysis is applied to the vehicles and the price of the vehicle " +
+               "and the price of the vehicle is predicted if possible. The difference between predicted and asked price " +
+               "can be used as a clue about financial attractiveness of the vehicle: we may be interested in finding underestimated vehicles. " +
+               "\n" +
+               "The vehicles with its properties are dumped as CSV file into '/dir-data/' directory."],
+
     "allscrape": ["a", "Scrape all items (time intensive), apply regression and dump"],
+
     "readcsv": ["r", "Read CSV for user inputs to be persisted, check for gone items"],
-    "regression": ["g", "Do analysis for the existing items"],
+
+    "regression": ["g", "Apply regression analysis to the vehicles. " +
+                   "\n" +
+                   "If possible, the price of the vehicle is predicted. The difference between predicted and asked price " +
+                   " should help to identified underestimated vehicles."],
+
     "dump": ["d", "Dump found items to CSV according to search pattern"],
+
     "help": ["h", "Print help for commands and legend for output"],
+
     "quit": ["q", "Quit the programm"],
 }
 
